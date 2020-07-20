9dba2595fd9b705c96947eb1c5ff116bab768cd2
Andrii Trofimchuk <trofimchuk.an@gmail.com>
2020-07-20 18:47:41 +0300

    Start with alerts

--- usermanager/frontend/src/components/App.js
+++ usermanager/frontend/src/components/App.js
@@ -1,16 +1,33 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
+// Components
+import Users from "../components/user/User";
+import Alert from "./alert/alert";
+
+// Redux
 import { Provider } from "react-redux";
 import store from "../store";
 
-import Users from "../components/user/User";
+const options = {
+  // you can also just use 'bottom center'
+  position: "top center",
+  timeout: 5000,
+  offset: "30px",
+  // you can also just use 'scale'
+  transition: "scale",
+};
 
 class App extends React.Component {
   render() {
     return (
       <Provider store={store}>
-        <Users />
+        <AlertProvider template={AlertTemplate} {...options}>
+          <Users />
+          <Alert />
+        </AlertProvider>
       </Provider>
     );
   }
